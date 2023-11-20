e5d0994548b377c375cc1bcb6000d5a6e0a3901d
Mieczysław Sobaś <m.sobas@fejm.pl>
2023-11-20 21:06:29 +0100

    Add project files.

---
+++ Blazor_M4_L12/Components/ModalDialog.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace Blazor_M4_L12.Components
+{
+    public partial class ModalDialog
+    {
+        [Parameter]
+        public string Name { get; set; }
+
+        [Parameter]
+        public string Content { get; set; }
+
+        [Parameter]
+        public EventCallback<MouseEventArgs> OnClickDelete { get; set; }
+        
+        [Parameter]
+        public EventCallback<MouseEventArgs> OnClickCancel { get; set; }
+    }
+}
---
+++ Blazor_M4_L12/Pages/Home.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components;
+
+namespace Blazor_M4_L12.Pages
+{
+    public partial class Home
+    {
+        private bool _modalDialogVisible = false;
+        private bool _stateVisible = false;
+
+        private void ShowModalDialog()
+        {
+            _stateVisible = false;
+            _modalDialogVisible = true;
+        }
+
+        private void OnClickCancel(MouseEventArgs e)
+        {
+            _modalDialogVisible = false;
+        }
+
+        private void OnClickDelete(MouseEventArgs e)
+        {
+            _stateVisible = true;
+            _modalDialogVisible = false;
+        }
+    }
+}
